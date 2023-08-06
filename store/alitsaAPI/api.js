08d28b5af4a1ca753a3cacc3e9c219a0b1dd53d6
Crowdbotics <engineering@crowdbotics.com>
2023-08-06 17:33:20 +0000

    Update store.

--- store/alitsaAPI/api.js
+++ store/alitsaAPI/api.js
@@ -1,72 +1,50 @@
-import axios from "axios";
+import axios from "axios"
 const alitsaAPI = axios.create({
   baseURL: "https://maracas-42640.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
-  return alitsaAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return alitsaAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function api_v1_login_create(payload) {
-  return alitsaAPI.post(`/api/v1/login/`, payload.data);
+  return alitsaAPI.post(`/api/v1/login/`, payload.data)
 }
-
 function api_v1_signup_create(payload) {
-  return alitsaAPI.post(`/api/v1/signup/`, payload.data);
+  return alitsaAPI.post(`/api/v1/signup/`, payload.data)
 }
-
 function rest_auth_login_create(payload) {
-  return alitsaAPI.post(`/rest-auth/login/`, payload.data);
+  return alitsaAPI.post(`/rest-auth/login/`, payload.data)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return alitsaAPI.get(`/rest-auth/logout/`);
+  return alitsaAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return alitsaAPI.post(`/rest-auth/logout/`);
+  return alitsaAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return alitsaAPI.post(`/rest-auth/password/change/`, payload.data);
+  return alitsaAPI.post(`/rest-auth/password/change/`, payload.data)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return alitsaAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return alitsaAPI.post(`/rest-auth/password/reset/`, payload.data)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return alitsaAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return alitsaAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
 }
-
 function rest_auth_registration_create(payload) {
-  return alitsaAPI.post(`/rest-auth/registration/`, payload.data);
+  return alitsaAPI.post(`/rest-auth/registration/`, payload.data)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return alitsaAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return alitsaAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return alitsaAPI.get(`/rest-auth/user/`);
+  return alitsaAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return alitsaAPI.put(`/rest-auth/user/`, payload.data);
+  return alitsaAPI.put(`/rest-auth/user/`, payload.data)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return alitsaAPI.patch(`/rest-auth/user/`, payload.data);
+  return alitsaAPI.patch(`/rest-auth/user/`, payload.data)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +60,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}

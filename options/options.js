2517aea648ffd01c7e413c2dae38ce92a9b72211
a litsa <amalia.litsa@gmail.com>
2023-08-17 14:15:28 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "a-litsa-42640",
-  url: "https://a-litsa-42640.botics.co",
-  api: "https://a-litsa-42640.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "a-litsa-42640",
+  "url": "https://a-litsa-42640.botics.co",
+  "api": "https://a-litsa-42640.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "addBankAccount"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
